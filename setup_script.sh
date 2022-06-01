38b1933d54d86aef98c3dce82cd9443e881f37e6
astrojl-test2 <juanlu+ghtest@hey.com>
2022-06-01 18:55:39 +0000

    Add setup_script.sh and explanation diagram

---
+++ setup_script.sh
@@ -0,0 +1,13 @@
+# Bash script
+# To run it, do:
+#
+# $ source setup_script.sh
+
+echo "Installing miniforge3..."
+pyenv install miniforge3
+
+export PYENV_VERSION=miniforge3
+
+conda create -n titanic310 python=3.10 --yes
+
+echo "Now you can run 'conda activate titanic310' on the terminal"
